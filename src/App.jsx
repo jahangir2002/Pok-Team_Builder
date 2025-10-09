46e1c51a59fcdf56614c8370140eaa25187f6f55
jahangir2002 <jahangirshaikh553@gmail.com>
2025-10-09 15:47:54 +0530

    Add motion dependency and integrate RotatingText component in App.jsx for enhanced branding

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import Navbar from "./components/Navbar";
+import RotatingText from "./components/RotatingText";
 
 const App = () => {
   return (
     <>
       <div className="w-full h-dvh bg-blue-200">
         <Navbar />
-        <div className="font-extrabold text-black text-5xl text-center py-5 leading-[1.5]">
-          PokéTeam <span className="bg-red-600 px-2 rounded-md text-stone-50">Builder</span>
+        <div className="font-extrabold text-black text-5xl text-center py-5 leading-[1.5] flex flex-col justify-center md:flex-row">
+          PokéTeam{" "}
+            <RotatingText
+              texts={["Builder", "Cards", "Is", "Cool!"]}
+              mainClassName="bg-red-600 px-2 rounded-md text-stone-50 px-2 sm:px-2 md:px-3 bg-cyan-300 text-black overflow-hidden py-0.5 sm:py-1 md:py-2 justify-center rounded-lg"
+              staggerFrom={"last"}
+              initial={{ y: "100%" }}
+              animate={{ y: 0 }}
+              exit={{ y: "-120%" }}
+              staggerDuration={0.025}
+              splitLevelClassName="overflow-hidden pb-0.5 sm:pb-1 md:pb-1"
+              transition={{ type: "spring", damping: 30, stiffness: 400 }}
+              rotationInterval={2000}
+            />
         </div>
-        
       </div>
     </>
   );
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,22 +3,23 @@ import React from "react";
 const Navbar = () => {
   return (
     <>
-  <div className="navbar bg-red-600 shadow-sm">
-    <div className="w-full max-w-[1400px] m-auto flex flex-col md:flex-row items-center justify-between gap-4 p-4">
-      <div className="flex-1 text-center md:text-left">
-        <a className="logo font-extrabold text-xl text-white">PokéTeam Builder</a>
+      <div className="navbar bg-red-600 shadow-sm">
+        <div className="w-full max-w-[1400px] m-auto flex flex-col md:flex-row items-center justify-between gap-4 p-4">
+          <div className="flex-1 text-center md:text-left">
+            <a className="logo font-extrabold text-xl text-white">
+              PokéTeam Builder
+            </a>
+          </div>
+          <div className="flex gap-2">
+            <input
+              type="text"
+              placeholder="Search"
+              className="input input-bordered w-32 md:w-48"
+            />
+          </div>
+        </div>
       </div>
-      <div className="flex gap-2">
-        <input
-          type="text"
-          placeholder="Search"
-          className="input input-bordered w-32 md:w-48"
-        />
-      </div>
-    </div>
-  </div>
-</>
-
+    </>
   );
 };
 

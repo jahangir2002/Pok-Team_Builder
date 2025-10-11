07248f7218b549ae83156bbe74cb2a99ddcc67ff
jahangir2002 <jahangirshaikh553@gmail.com>
2025-10-11 12:57:55 +0530

    Add API fetching functionality to Pokemon component and implement useEffect for data retrieval

--- src/Pokemon.jsx
+++ src/Pokemon.jsx
@@ -1,8 +1,44 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Pokemon = () => {
+
+    const API = "https://pokeapi.co/api/v2/pokemon/?limit=24";
+
+    const fetchpokemon =  async()=> {
+        try {
+            const res = await fetch(API);
+            const data = await res.json();
+            // console.log(data);
+
+            const detailpokemon = data.results.map(async(currentpokemon)=> {
+                // console.log(currentpokemon.url)
+
+                const currentres = await fetch(currentpokemon.url)
+                const currentdata = await currentres.json();
+                console.log(currentdata);
+            })
+
+
+
+        } catch (error) {
+            console.log(error);
+            
+        }
+    }
+
+
+
+    useEffect(() => {
+        fetchpokemon();
+        
+    })
+
   return (
+
+    <>
     <div className='max-w-[1400px] m-auto'>Pokemon</div>
+    </>
+
   )
 }
 
